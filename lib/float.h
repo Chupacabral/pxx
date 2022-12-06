c12af4db6ff04a404d6b4677914a9922f02baefa
chupacabral <chupacabral.dev@gmail.com>
2022-12-06 07:21:07 -0700

    Added << operator overload to Float

--- lib/float.h
+++ lib/float.h
@@ -34,6 +34,17 @@ namespace pxx {
         m_object = PyFloat_FromDouble(n);
       }
 
+      /// @brief Overloaded << operator for output streams. A pxx Float will
+      ///        convert to a double when inserting into stream, which will
+      ///        allow it to work with IO manipulators and formatters better.
+      /// @param stream The output stream to print to.
+      /// @param item The pxx Float to output.
+      /// @return The stream with the float printed onto it.
+      friend std::ostream& operator<<(std::ostream& stream, const Float& item) {
+        stream << item.to_double();
+        return stream;
+      }
+
       // TODO: PyFloat_GetInfo after defining NamedTuple (or whatever structseq
       //       is).
   };
